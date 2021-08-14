622e9590cca3139347bb591f822f39a4f9d446d0
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2021-08-14 18:30:50 -0400

    Update 8_imports.py

--- 8_imports.py
+++ 8_imports.py
@@ -10,6 +10,9 @@ print(math.pi, math.e, math.tau)
 
 # Common functions (ceil, sqrt, log10, pow, gcd)
 from math import ceil, sqrt, log10, pow, gcd
+print(ceil(3/2))
+print(sqrt(9))
+print(log10(1000))
 print(pow(2, 100))
 print(pow(2, 9))
 print(gcd(12, 15))
